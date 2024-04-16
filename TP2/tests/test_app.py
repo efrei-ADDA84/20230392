ee88c337533e55c904d2c0500fa0be53701140f1
jaoox <joao.belintane@gmail.com>
2024-04-16 15:25:15 +0200

    Adding the unittest in CI/CD Pipeline

--- TP2/tests/test_app.py
+++ TP2/tests/test_app.py
@@ -6,7 +6,7 @@ from flask_testing import TestCase
 # Add the src directory to sys.path to find the appWeather module
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from src.gettingWeather import appWeather  # Import the Flask app
+from src.gettingWeather import appWeather
 
 class TestWeatherApp(TestCase):
     def create_app(self):
@@ -16,7 +16,6 @@ class TestWeatherApp(TestCase):
         return appWeather
 
     def test_weather_route_exists(self):
-        # Ensure the route is accessible
         response = self.client.get('/gettingWeather?lat=40.712776&lon=-74.005974&API_KEY=048a8f361dd3ae83d166d41cd7767b74')
         self.assertEqual(response.status_code, 200)
 
